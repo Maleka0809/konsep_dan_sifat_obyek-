cd9723686db5722bfe64c8ed7679a10743c00b85
Maleka0809 <malekaghaniya0809@gmail.com>
2025-10-01 21:33:09 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,23 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        
         Dosen d1 = new Dosen("Pak FB");
         Dosen d2 = new Dosen("Bu SW");
 
-        
         MataKuliah mk1 = new MataKuliah("A", "PWeb", d1);
         MataKuliah mk2 = new MataKuliah("C", "KPPL", d2);
 
-    
         System.out.print("Masukkan nama mahasiswa: ");
         String nama = sc.nextLine();
-        System.out.print("Masukkan NIM mahasiswa: ");
-        String nim = sc.nextLine();
+        System.out.print("Masukkan NRP mahasiswa: ");
+        String nrp = sc.nextLine();
 
-        Mahasiswa mhs = new Mahasiswa(nama, nim);
+        Mahasiswa mhs = new Mahasiswa(nama, nrp);
 
-        
         System.out.println("Pilih mata kuliah yang ingin diambil:");
         System.out.println("1. " + mk1);
         System.out.println("2. " + mk2);
@@ -31,9 +27,10 @@ public class Main {
             mhs.tambahMatkul(mk1);
         } else if (pilihan == 2) {
             mhs.tambahMatkul(mk2);
+        } else {
+            System.out.println("Pilihan tidak valid.");
         }
 
-        
         System.out.println();
         mhs.tampilkanKRS();
 
